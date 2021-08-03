219766d101ebb088fdd2c9b6aa62f48768e24f74
tgoldsack1 <tgoldsack1@sheffield.ac.uk>
2021-08-03 08:44:43 +0100

    added longsumm data

---
+++ my_data/longsumm/parse_longsumm.py
@@ -0,0 +1,85 @@
+'''
+Parses longsummm dataset json file to make format input for the given model.
+Author: Tomas Goldsack
+'''
+
+import os, json, re
+from nltk.tokenize import sent_tokenize
+
+longsumm_file = open("./my_longsumm_train.json")
+longsumm_dict = json.load(longsumm_file)
+longsumm_file.close()
+
+
+## pointer-generator-pytorch-allennlp
+# Desired input format (json)
+# { 
+#   "article_lines": List[str],
+#   "summary_lines": List[str]
+# }
+
+
+# Get 2 versions of data: 
+# 1 using the abstract/introduction/conclusions (AIC)
+# 1 at section-level (test only)
+AIC_datafile = open("./my_longsumm_train_AIC.jsonl", "w")
+
+for paper_id, paper_dict in longsumm_dict.items():
+  AIC_data = {}
+
+  article_lines = []
+  
+  summary_sents = paper_dict['Y']['summary']
+  input_dict = paper_dict['X'] 
+  # X format:
+  # {
+  #   "sections": [{ "heading": str, "text": str }], 
+  #   "year": int,
+  #   "references": [{...}],
+  #   "id" : str,
+  #   "authors" : [{ "name": str, "affiliations": str }]
+  #   "abstractText": str,
+  #   "title": str
+  # }
+
+  AIC_data["summary_lines"] = summary_sents
+
+  # A section (abstract)
+  if "abstractText" in input_dict.keys():
+    article_lines = article_lines + sent_tokenize(input_dict["abstractText"])
+
+
+    # Get IC sections
+    if 'sections' in input_dict.keys() and len(input_dict['sections']) > 2:
+      heading_sections = [s for s in input_dict['sections'] if "heading" in s.keys()]
+
+      # I section (intro)
+      intro_matches= [x for x in heading_sections if re.search("intro*", x["heading"].lower())]
+      if intro_matches:
+        intro = intro_matches[0]['text'] # task first match
+        if len(intro_matches) > 1:
+          print(f'MUTLIPLE INTRO MATCHES: {input_dict["id"]}')
+      else: 
+        intro = input_dict['sections'][0]['text']
+
+      article_lines = article_lines + sent_tokenize(intro)
+      
+      # C section (conclusion)
+      conc_matches= [x for x in heading_sections if (re.search("concl*", x["heading"].lower()) or \
+        re.search("discuss*", x["heading"].lower()))]
+
+      if conc_matches:
+        conc = conc_matches[-1]['text'] # take last match
+        if len(conc_matches) > 1:
+          print(f'MUTLIPLE CONC MATCHES: {input_dict["id"]}')
+          print([x['heading'] for x in conc_matches])
+      else: 
+        conc = input_dict['sections'][-1]['text'] if input_dict['sections'][-1] != intro else "" 
+
+      article_lines = article_lines + sent_tokenize(conc)
+
+      # add to output file
+      AIC_datafile.write(json.dumps(AIC_data))
+
+AIC_datafile.close()
+  
